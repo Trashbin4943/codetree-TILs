6353b1f5c18d370d8ad64b1b27e729bb055f06a4
Trashbin4943 <anthonyhwang415@gmail.com>
2024-06-30 14:44:48 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240630/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,29 @@
+n=int(input())
+numli=list(map(int,input().split()))
+
+for i in range (n):
+    least=numli[0]
+    idxx=[]
+    for j in numli[1:n+1]:
+        if j>=least:
+            least=j
+
+idxx.append(numli.index(least))
+if numli.count(least)>=2:
+    print(idxx[0]+1,end=" ")
+else:
+    print(idxx[0]+1,end=" ")
+
+n=idxx[0]
+least2=numli[0]
+for k in numli[1:n]:
+    if k>least2:
+        least2=k
+
+
+idxx2=[]
+idxx2.append(numli.index(k))
+if numli.count(least2)>=2:
+    print(idxx2[0]+1)
+else:
+    print(idxx2[0]+1)
