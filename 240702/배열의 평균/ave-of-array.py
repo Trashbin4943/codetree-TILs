da39e658649d498039cdcd82bb467990d7be5490
Trashbin4943 <anthonyhwang415@gmail.com>
2024-07-02 00:16:19 +0900

    Title: 배열의 평균, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240702/배열의 평균/ave-of-array.py
@@ -0,0 +1,22 @@
+listing=[]
+for i in range (2):
+    listing.append(list(map(int,input().split())))
+
+for j in range (2):
+    garo=0
+    for k in range (4):
+        garo+=listing[j][k]
+    avv=garo/4
+    print(f"{avv:.1f}",end=" ")
+print()
+
+for m in range (4):
+    sero=0
+    for n in range (2):
+        sero+=listing[n][m]
+    avv2=sero/2
+    print(f"{avv2:.1f}",end=" ")
+print()
+
+sumval=sum(listing[0])+sum(listing[1])
+print(sumval/8)
