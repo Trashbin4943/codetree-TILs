257245e52a6d8b11dd1a5eae3249c2185f75fea6
Trashbin4943 <anthonyhwang415@gmail.com>
2024-07-01 23:01:36 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240701/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240701/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,16 +1,29 @@
 n=int(input())
 price=list(map(int,input().split()))
+
 buy=min(price)
 m=price.index(buy)
 
-margin=[]
-for i in price[m+1:n+1:1]:
+marg1=[]
+for i in price[m+1::]:
     if i-buy>0:
-        margin.append(i-buy)
-    else:
-        pass
+        marg1.append(i-buy)
 
-if margin!=[]:
-    print(max(margin))
+sell=max(price)
+marg2=[]
+n=price.index(sell)
+for j in price[0:n]:
+    if sell-j>0:
+        marg2.append(sell-j)
+
+if marg1 and marg2:
+    if max(marg1)>max(marg2):
+        print(max(marg1))
+    elif max(marg1)<max(marg2):
+        print(max(marg2))
+elif marg1:
+    print(max(marg1))
+elif marg2:
+    print(max(marg2))
 else:
     print(0)
