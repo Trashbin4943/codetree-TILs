18cbbf48ef35380f9f0704e4a8a3d8124fd547b1
Trashbin4943 <anthonyhwang415@gmail.com>
2025-01-03 00:41:23 +0900

    Title: 친근하지 않은 수, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 250103/친근하지 않은 수/unfriendly-number.java
+++ 250103/친근하지 않은 수/unfriendly-number.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int cnt=0;
-        for (int i=1; i<=10; i++) {
+
+        Scanner s = new Scanner (System.in);
+        int n = s.nextInt();
+
+        for (int i=1; i<=n; i++) {
             if (i%2==0 || i%3==0 || i%5==0) {
                 continue;
             }
