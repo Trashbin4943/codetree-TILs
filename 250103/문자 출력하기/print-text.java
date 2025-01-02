89fe3ed242dac3485abb9f4f63c10533e1bb5eb6
Trashbin4943 <anthonyhwang415@gmail.com>
2025-01-03 00:14:52 +0900

    Title: 문자 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250103/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        char a= s.next().charAt(0);
+
+        for (int i=1; i<=8; i++) {
+            System.out.print(a);
+        }
+
+
+    }
+}
