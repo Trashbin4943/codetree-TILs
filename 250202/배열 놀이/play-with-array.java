89ab0ce25f99efd4851836a92f5e7efb1509336c
Trashbin4943 <anthonyhwang415@gmail.com>
2025-02-02 16:36:46 +0900

    Title: 배열 놀이, Time: 167ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250202/배열 놀이/play-with-array.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] num = new int[n];
+
+        for (int i=0;i<n;i++){
+            num[i]=sc.nextInt();
+        }
+
+        for (int j=1;j<=q;j++){
+            int questcode = sc.nextInt();
+
+            if (questcode==1){
+                int idx = sc.nextInt();
+                System.out.println(num[idx-1]);
+            }
+
+            else if (questcode==2){
+                int b = sc.nextInt();
+                int id=0;
+
+                for (int i=0;i<n;i++){
+                    if (num[i]==b){
+                        id=i;
+                        break;
+                    }
+                }
+                System.out.println(id+1);
+            }
+
+            else if (questcode==3){
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+
+                for (int i=s;i<=e;i++){
+                    System.out.print(num[i-1]+" ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
