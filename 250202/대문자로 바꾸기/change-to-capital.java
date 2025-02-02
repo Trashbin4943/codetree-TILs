fd1c4106c9b1a42d2107a973eed420f0db2edc39
Trashbin4943 <anthonyhwang415@gmail.com>
2025-02-02 17:33:21 +0900

    Title: 대문자로 바꾸기, Time: 103ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250202/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i=0;i<5;i++){
+            for(int j=0;j<3;j++){
+                char input = sc.next().charAt(0);
+                int asc = (int)input;
+                if ((97<=asc)&&(asc<122)){
+                    System.out.print((char)(asc-32)+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
