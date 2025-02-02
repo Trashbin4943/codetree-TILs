119a9243668564673530cca2ea2b2bd30e50ff0d
Trashbin4943 <anthonyhwang415@gmail.com>
2025-02-02 17:34:25 +0900

    Title: 대문자로 바꾸기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 250202/대문자로 바꾸기/change-to-capital.java
+++ 250202/대문자로 바꾸기/change-to-capital.java
@@ -8,7 +8,7 @@ public class Main {
             for(int j=0;j<3;j++){
                 char input = sc.next().charAt(0);
                 int asc = (int)input;
-                if ((97<=asc)&&(asc<122)){
+                if ((97<=asc)&&(asc<=122)){
                     System.out.print((char)(asc-32)+" ");
                 }
             }
