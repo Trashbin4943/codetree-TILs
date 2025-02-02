98f7eff33229dd979aed12b26a14b08c9896390c
Trashbin4943 <anthonyhwang415@gmail.com>
2025-02-02 18:04:40 +0900

    Title: 배열로 사각형 만들기, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250202/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] sq = new int[5][5];
+        for (int i=0;i<5;i++){
+            sq[0][i]=1;
+            sq[i][0]=1;
+        }
+
+        for (int i=1;i<5;i++){
+            for (int j=1;j<5;j++){
+                sq[i][j]=sq[i-1][j]+sq[i][j-1];
+            }
+        }
+
+        for (int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(sq[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
